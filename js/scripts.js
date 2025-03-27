c731b52ba3e137d094514a2235e9b532f1b429b1
unknown <pablodoebber28@gmail.com>
2025-03-27 14:59:13 +0000

    Resolvi mais botões.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-fetch('https://www.instagram.com/doebber.tech/?__a=1')
-  .then(response => response.json())
-  .then(data => {
-      let followers = data.graphql.user.edge_followed_by.count;
-      console.log("Seguidores:", followers);
-  })
-  .catch(error => console.error("Erro ao puxar seguidores:", error));
+let linkPortfolio = document.querySelector('#link-portfolio')
+
+linkPortfolio.addEventListener('click', function() {
+  event.preventDefault()
+  alert('Em manutenção!')
+})
